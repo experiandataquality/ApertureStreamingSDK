f971ec5c8ee047226d14383bc70964b4ca2a5e39
Shukri Armizi Md Hanif <ShukriArmizi.MdHanif@experian.com>
2020-01-08 11:45:16 +0800

    Add snapshot repository

--- java/client/src/main/java/com/experian/aperture/streaming/client/handler/DefaultConnectionFailureHandlerBuilder.java
+++
@@ -1,8 +0,0 @@
-package com.experian.aperture.streaming.client.handler;
-
-final class DefaultConnectionFailureHandlerBuilder implements  ConnectionFailureHandlerBuilder {
-    @Override
-    public ConnectionFailureHandler build() {
-        return new DefaultConnectionFailureHandler();
-    }
-}
